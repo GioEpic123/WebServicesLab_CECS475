fe4e817aa1b3670ecef37586a1abdfeecf5f784d
Remo von Arx <remo.vonarx@students.fhnw.ch>
2022-09-21 15:39:11 -0700

    Cleanup code

--- FlickrViewer/FlickrViewer/FlickrResult.cs
+++ FlickrViewer/FlickrViewer/FlickrResult.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlickrViewer
+﻿namespace FlickrViewer
 {
    class FlickrResult
    {
       public string Title { get; set; }
-      public string URL { get; set; }
+      public string Url { get; set; }
    }
 }
