5299b9576b21452054ef674b04f50694414df076
VN <personallyvnguyen@gmail.com>
2022-09-25 14:32:18 -0700

    removed unnecessary usings and cleaned up comments

--- FlickrViewer/FlickrViewer/FlickrResult.cs
+++ FlickrViewer/FlickrViewer/FlickrResult.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlickrViewer
+﻿namespace FlickrViewer
 {
-   class FlickrResult
+    class FlickrResult
    {
       public string Title { get; set; }
       public string URL { get; set; }
--- FlickrViewer/FlickrViewer/FlickrViewerForm.Designer.cs
+++ FlickrViewer/FlickrViewer/FlickrViewerForm.Designer.cs
@@ -1,33 +1,33 @@
 ﻿namespace FlickrViewer
 {
-   partial class FlickrViewerForm
-   {
-      /// <summary>
-      /// Required designer variable.
-      /// </summary>
-      private System.ComponentModel.IContainer components = null;
+    partial class FlickrViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-      /// <summary>
-      /// Clean up any resources being used.
-      /// </summary>
-      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-      protected override void Dispose( bool disposing )
-      {
-         if ( disposing && ( components != null ) )
-         {
-            components.Dispose();
-         }
-         base.Dispose( disposing );
-      }
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-      #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-      /// <summary>
-      /// Required method for Designer support - do not modify
-      /// the contents of this method with the code editor.
-      /// </summary>
-      private void InitializeComponent()
-      {
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
             this.label1 = new System.Windows.Forms.Label();
             this.inputTextBox = new System.Windows.Forms.TextBox();
             this.pictureBox = new System.Windows.Forms.PictureBox();
@@ -39,33 +39,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(12, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 25);
+            this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Flickr search tags here:";
             // 
             // inputTextBox
             // 
-            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.inputTextBox.Location = new System.Drawing.Point(326, 25);
-            this.inputTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+               | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(163, 13);
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(852, 31);
+            this.inputTextBox.Size = new System.Drawing.Size(428, 20);
             this.inputTextBox.TabIndex = 1;
-            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
             // 
             // pictureBox
             // 
-            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox.Location = new System.Drawing.Point(326, 77);
-            this.pictureBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+               | System.Windows.Forms.AnchorStyles.Left)
+               | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(163, 40);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1006, 1008);
+            this.pictureBox.Size = new System.Drawing.Size(503, 524);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 3;
             this.pictureBox.TabStop = false;
@@ -73,10 +69,9 @@
             // searchButton
             // 
             this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.searchButton.Location = new System.Drawing.Point(1194, 21);
-            this.searchButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.searchButton.Location = new System.Drawing.Point(597, 11);
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(140, 44);
+            this.searchButton.Size = new System.Drawing.Size(70, 23);
             this.searchButton.TabIndex = 4;
             this.searchButton.Text = "Search";
             this.searchButton.UseVisualStyleBackColor = true;
@@ -85,41 +80,38 @@
             // imagesListBox
             // 
             this.imagesListBox.FormattingEnabled = true;
-            this.imagesListBox.ItemHeight = 25;
-            this.imagesListBox.Location = new System.Drawing.Point(30, 77);
-            this.imagesListBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.imagesListBox.Location = new System.Drawing.Point(15, 40);
             this.imagesListBox.Name = "imagesListBox";
-            this.imagesListBox.Size = new System.Drawing.Size(280, 1004);
+            this.imagesListBox.Size = new System.Drawing.Size(142, 524);
             this.imagesListBox.TabIndex = 5;
             this.imagesListBox.SelectedIndexChanged += new System.EventHandler(this.imagesListBox_SelectedIndexChanged);
             // 
-            // FlickrViewerForm
+            // FickrViewerForm
             // 
             this.AcceptButton = this.searchButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1358, 1104);
+            this.ClientSize = new System.Drawing.Size(679, 574);
             this.Controls.Add(this.imagesListBox);
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.Name = "FlickrViewerForm";
+            this.Name = "FickrViewerForm";
             this.Text = "Flickr Viewer";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
-      }
+        }
 
-      #endregion
+        #endregion
 
-      private System.Windows.Forms.Label label1;
-      private System.Windows.Forms.TextBox inputTextBox;
-      private System.Windows.Forms.PictureBox pictureBox;
-      private System.Windows.Forms.Button searchButton;
-      private System.Windows.Forms.ListBox imagesListBox;
-   }
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListBox imagesListBox;
+    }
 }
 
